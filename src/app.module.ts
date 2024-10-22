bc03ef43cf52b46ce72a827eb95a8e14ffa282b2
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2024-10-22 16:35:51 -0600

    Initialize NestJS project and initial db configuration

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { DBOptions } from '../db.datasourceoptions';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        const dbOptions: TypeOrmModuleOptions = {};
+        Object.assign(dbOptions, DBOptions);
+        return dbOptions;
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
