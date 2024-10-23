1a43d38f06c8c1788b7d103b6a573cc7fd452f25
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2024-10-23 14:46:40 -0600

    implementation book filtering

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -6,11 +6,12 @@ import {
   Param,
   Delete,
   Put,
+  Query,
+  BadRequestException,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { Book } from './book.entity';
-
 
 @Controller('book')
 export class BookController {
@@ -19,6 +20,40 @@ export class BookController {
   @Get()
   async getAllBooks(): Promise<Book[]> {
     return await this.bookService.getAll();
+  }
+
+  @Get('by-genre')
+  async filterBooksByGenre(@Query('genre') genre: string): Promise<Book[]> {
+    if (!genre || typeof genre !== 'string') {
+      throw new BadRequestException('Genre is required and must be a string.');
+    }
+    return this.bookService.filterBooksByGenre(genre);
+  }
+
+  @Get('by-title')
+  async filterBooks(@Query('title') title: string): Promise<Book[]> {
+    if (!title || typeof title !== 'string') {
+      throw new BadRequestException('Title is required and must be a string.');
+    }
+    return this.bookService.filterBooksByTitle(title);
+  }
+
+  @Get('by-author')
+  async filterBooksByAuthor(@Query('author') author: string): Promise<Book[]> {
+    if (!author || typeof author !== 'string') {
+      throw new BadRequestException('Author is required and must be a string.');
+    }
+    return this.bookService.filterBooksByAuthor(author);
+  }
+
+  @Get('by-published-year')
+  async filterBooksByPublishedYear(
+    @Query('publishedYear') publishedYear: string,
+  ): Promise<Book[]> {
+    if (!publishedYear) {
+      throw new BadRequestException('Published year is required.');
+    }
+    return this.bookService.filterBooksByPublishedYear(publishedYear);
   }
 
   @Get(':id')
